8b4252cc23f60e3208a8e04224735da7162daf95
HarrisonDebz <harrisonmalisawa16@gmail.com>
2025-11-22 04:43:12 +0200

    Added .gitignore file

--- obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12649e81c2ea98c710ea044848c9e20c783a9e8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
